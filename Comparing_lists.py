e71069b340c3df7b11f2e0c954a47f7980da4cc4
Josh-Cruz <josh@digitalmonestary.com>
2018-07-05 10:10:33 -0700

    wrote code to compare 2 lists

---
+++ Comparing_lists.py
@@ -0,0 +1,31 @@
+# Write a program that compares two lists and prints a message depending on if the inputs are identical or not.
+
+# Your program should be able to accept and compare two lists:
+#  list_one and list_two. If both lists are identical print "The lists are the same". 
+# If they are not identical print "The lists are not the same." Try the following test cases for lists one and two:
+
+# list_one = [1, 2, 5, 6, 2]
+# list_two = [1, 2, 5, 6, 3]
+# list_one = [1, 2, 5, 6, 5]
+# list_two = [1, 2, 5, 6, 5, 3]
+# list_one = [1, 2, 5, 6, 5, 16]
+# list_two = [1, 2, 5, 6, 5]
+list_one = ['celery', 'carrots', 'bread', 'milk']
+list_two = ['celery', 'carrots', 'bread', 'cream']
+
+def list_compare (x,y):
+    answer = "" 
+    if len(x) != len(y):
+         return "The lists are not the same."
+    else: 
+        for i in range(len(y)): 
+            if y[i] != x[i]:
+               answer = "The lists are not the same."
+               break
+            else:
+               answer = "The lists are identical!"
+        return answer
+
+
+
+print (list_compare(list_one, list_two))
