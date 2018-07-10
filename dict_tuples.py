b59925b3332ca9efb6ee701c9e3a26b6a86b9cd3
Josh-Cruz <josh@digitalmonestary.com>
2018-07-10 11:33:04 -0700

    working with dictionaries in pythonm

---
+++ dict_tuples.py
@@ -0,0 +1,25 @@
+# Assignment: Dictionary in, tuples out
+# Write a function that takes in a dictionary and returns a list of tuples where the first tuple item is the key and the second is the value. Here's an example:
+
+#     # function input
+my_dict = {
+    "Speros": "(555) 555-5555",
+    "Michael": "(999) 999-9999",
+    "Jay": "(777) 777-7777"
+}
+# #function output
+# [("Speros", "(555) 555-5555"), ("Michael", "(999) 999-9999"), ("Jay", "(777) 777-7777")]
+
+def dict_to_tuple(dictn):
+    dictn.items()
+    print [(v, k) for v, k in dictn.iteritems()]
+
+
+    
+    
+    # new_dict = {}
+    # for x in dictn:
+    #     for y in dictn:
+    #        print new_dict[x].append((y))
+
+print(dict_to_tuple(my_dict))
