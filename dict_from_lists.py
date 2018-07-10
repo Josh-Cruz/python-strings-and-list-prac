6914965761c5aa753eacaf7e30a33d2ee17c5606
Josh-Cruz <josh@digitalmonestary.com>
2018-07-10 11:48:33 -0700

    made a dictionary from 2 lists

---
+++ dict_from_lists.py
@@ -0,0 +1,22 @@
+# Assignment: Making Dictionaries
+# Create a function that takes in two lists and creates a single dictionary.
+#  The first list contains keys and the second list contains the values.
+#  Assume the lists will be of equal length.
+
+# Your first function will take in two lists containing some strings. Here are two example lists:
+
+name = ["Anna", "Eli", "Pariece", "Brendan", "Amy", "Shane", "Oscar"]
+favorite_animal = ["horse", "cat", "spider", "giraffe", "ticks", "dolphins", "llamas"]
+
+
+def make_dict(list1, list2):
+    import itertools
+    if len(list1) >= len(list2):
+        new_dict = dict(itertools.izip(list1, list2))
+    else:
+        new_dict = dict(itertools.izip(list2, list1))
+
+    return new_dict
+
+
+print(make_dict(name,favorite_animal))
